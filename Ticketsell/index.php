d26564a1dfdd94b0c9c6bff13b486ad17812e983
Luisa Haase <luisa.haase@plentymarkets.com>
2020-09-23 11:14:54 +0200

    Bootstrap Anpassungen

--- Ticketsell/index.php
+++ Ticketsell/index.php
@@ -29,13 +29,13 @@ require_once ('UserService.php');
                 <div class="col-md-4">
 
                 </div>
-                <div class="col-md-4 bg-light">
+                <div class="col-md-4 bg-light p-3 rounded">
                     <form action="login.php" method="post">
-                        Login
+                        <h1 class="text-center">Login</h1>
                         <br/>
-                        <input type="text" name="userName" placeholder="Name"/><br>
-                        <input type="password" name="password" placeholder="Passwort"/><br>
-                        <input type="Submit" name="login" placeholder="Anmelden"/>
+                        <input class="m-2 form-control" type="text" name="userName" placeholder="Name"/><br>
+                        <input class="m-2 form-control" type="password" name="password" placeholder="Passwort"/><br>
+                        <button type="button" class="btn btn-primary align-content-center" type="submit">Anmelden</button>
                     </form>
                 </div>
                 <div class="col-md-4">
@@ -46,7 +46,10 @@ require_once ('UserService.php');
         </div>
 
 
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
     </body>
